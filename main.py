e6936097c534e26c2407ec44322faa45cb37257c
Vitaliy375 <yacenko375@gmail.com>
2026-01-09 12:23:36 +0400

    add project

---
+++ main.py
@@ -0,0 +1,6 @@
+from src.masks import get_mask_card_number, get_mask_account
+
+if __name__ == "__main__":
+    print(get_mask_card_number(8796987605679876))
+    print(get_mask_account(9786378598677354))
+
---
+++ src/masks.py
@@ -0,0 +1,30 @@
+def get_mask_card_number(number: int) -> str:
+    """
+    Преобразование числа в маску.
+
+    :param number: int принимает целочисленный вид данных
+    :return: str возвращает строку в формате XXXX XX** **** XXXX
+    """
+    res_fin = []
+    conv_str = str(number)
+    count_stars = len(conv_str[7:-5]) * "*"
+    join_number = f"{conv_str[:6]}{count_stars}{conv_str[-4:]}"
+    for i in range(0, len(join_number), 4):
+        res_fin.append(join_number[i: i + 4])
+    return " ".join(res_fin)
+
+
+def get_mask_account(number: int) -> str:
+    """
+    Преобразование числа в маску счета.
+
+    :param number: int принимает целочисленный вид данных
+    :return: str возвращает строку в формате **XXXX
+    """
+    conv_str = str(number)
+    return f"**{conv_str[-4:]}"
+
+
+if __name__ == "__main__":
+    print(get_mask_card_number(879667987605679876))
+    print(get_mask_account(9786378598677354))
