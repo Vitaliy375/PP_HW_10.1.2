701ee62b1bcb724730b24dae56e38c437e4dd42a
Vitaliy375 <yacenko375@gmail.com>
2026-02-12 20:26:02 +0400

    fixes function and tests

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,17 @@
-def get_mask_card_number(number: str) -> str:
+def get_mask_card_number(number: int) -> str:
     """
     Преобразование числа в маску.
 
-    :param number: str принимает целочисленный вид данных
+    :param number: int принимает целочисленный вид данных
     :return: str возвращает строку в формате XXXX XX** **** XXXX
     """
-    res_fin = []
+    if number < 0 or not isinstance(number, int):
+        raise ValueError("Неверный номер карты")
     conv_str = str(number)
+    if len(conv_str) < 10:
+        raise ValueError("Номер карты слишком короткий")
+    if number < 0: raise ValueError("Номер не может быть отрицательным")
+    res_fin = []
     count_stars = len(conv_str[6:-4]) * "*"
     join_number = f"{conv_str[:6]}{count_stars}{conv_str[-4:]}"
     for i in range(0, len(join_number), 4):
@@ -14,11 +19,11 @@ def get_mask_card_number(number: str) -> str:
     return " ".join(res_fin)
 
 
-def get_mask_account(number: str) -> str:
+def get_mask_account(number: int) -> str:
     """
     Преобразование числа в маску счета.
 
-    :param number: str Принимает целочисленный вид данных
+    :param number: int Принимает целочисленный вид данных
     :return: str возвращает строку в формате **XXXX
     """
     conv_str = str(number)
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -7,7 +7,7 @@ from src.masks import get_mask_card_number, get_mask_account
     "number, expected",
     [
         (1234567890123456, "1234 56** **** 3456"),  # 16 цифр
-        (1234567890123456789, "123456 ***** 6789"),  # 19 цифр
+        (1234567890123456789, "1234 56** **** ***6 789"),  # 19 цифр
         (1234567890, "1234 5678 90"),               # 10 цифр (минимум)
         (9999999999999999, "9999 99** **** 9999"),   # Все 9
     ]
@@ -18,12 +18,12 @@ def test_get_mask_card_number_valid(number: int, expected: str):
 
 def test_get_mask_card_number_negative():
     """Отрицательное число для карты."""
-    with pytest.raises(IndexError):
+    with pytest.raises(ValueError):
         get_mask_card_number(-1234567890123456)
 
 def test_get_mask_card_number_too_short():
     """Слишком короткий номер (менее 10 цифр)."""
-    with pytest.raises(IndexError):
+    with pytest.raises(ValueError):
         get_mask_card_number(12345)
 
 ## Тесты для get_mask_account
@@ -31,7 +31,7 @@ def test_get_mask_card_number_too_short():
     "number, expected",
     [
         (12345678, "**5678"),
-        (123, "**0123"),      # Менее 4 цифр
+        (123, "**123"),      # Менее 4 цифр
         (73654108430135874305, "**4305"),  # Длинный счет
     ]
 )
